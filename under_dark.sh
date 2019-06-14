bfa36211fc95786a9e6736fd917acd1193dc6d91
Talapus <evan.hamlin1@gmail.com>
2019-06-14 14:39:26 -0700

    under_dark.sh: shell version

---
+++ under_dark.sh
@@ -0,0 +1,14 @@
+
+#!/usr/bin/env bash
+
+# Generic
+#	  Cavernous
+#	  Cramped
+#	  Volcanic
+#	  Mushroom
+#	  Aquatic
+#	  Ruin
+#	  Boneyard
+#   Pit
+#   Chasm
+#   Giant stalagtites and stalagmites
