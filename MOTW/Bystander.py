a5024ef4cacf32f6d60f40d1e44d43c796e66935
Talapus <evan.hamlin1@gmail.com>
2020-12-09 14:58:27 -0800

    MOTW: initial commit

---
+++ MOTW/Bystander.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+#from sys import argv
+#from sys import stdout
+#from random import randint
+#from random import shuffle
+from faker import Faker
+fake = Faker()
+
+print(fake.name())
